bb94e57f6cdea1cc46667429311f6fa4b36bb1ba
Inal Mahpud <inalmahpud1@gmail.com>
2023-10-22 20:36:50 +0700

    add static server

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const router = require('./route/index.js')
 const app = express()
+const path = require('path')
 
 const port = 8000
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
+app.use('/images', express.static(path.join(__dirname, '../uploads')));
 app.use(router)
 app.listen(port, () => {
     console.log(`server berjalan di port ${port}`)
